5964ddfe95247245e9d8a13e4c8278d70ec24e43
jantonio2 <jesusrochavedia@gmail.com>
2021-05-27 18:39:53 -0400

    Functions

---
+++ Ejercicios/typescript/functions.ts
@@ -0,0 +1,23 @@
+//Funciones
+function add(a:number, b:number):number {
+  return a + b;
+}
+
+const sum = add(4, 5);
+
+function createAdder(a: number):(number) => number{
+  return function (b: number) {
+    return b + a;
+  }
+}
+
+const addFour = createAdder(4);
+const fourPlus6 = addFour(6);
+
+function fullName(firstName:string, lastName?:string):string {
+  return `${firstName} ${lastName}`;
+}
+
+const antonio = fullName('Antonio');
+
+console.log(antonio);
